a6752ad7f939ce193e29d9b34418b3712fd4728d
shinseongsu <tlstjdtn321@naver.com>
2026-02-28 16:45:38 +0900

    feat: 수정

--- apps/server/src/game/domain/data/quotes.ts
+++ apps/server/src/game/domain/data/quotes.ts
@@ -13,7 +13,7 @@ const quotes: Quote[] = [
   { id: 3, text: "어이가 없네?", actor: "유아인", character: "조태오", movie: "베테랑", audio: "se_003.m4a" },
   { id: 4, text: "살려는 드릴게.", actor: "박성웅", character: "이중구", movie: "신세계", audio: "se_004.m4a" },
   { id: 5, text: "누구냐, 넌.", actor: "최민식", character: "오대수", movie: "올드보이", audio: "se_005.m4a" },
-  { id: 6, text: "금이빨 빼고 모조리 씹어먹어줄게.", actor: "원빈", character: "차태식", movie: "아저씨", audio: "se_006.m4a" },
+  { id: 6, text: "이거 방탄유리야", actor: "원빈", character: "차태식", movie: "아저씨", audio: "se_006.m4a" },
   { id: 7, text: "너나 잘하세요.", actor: "이영애", character: "이금자", movie: "친절한 금자씨", audio: "se_007.m4a" },
   { id: 8, text: "니가 가라 하와이.", actor: "장동건", character: "한동수", movie: "친구", audio: "se_008.m4a" },
   { id: 9, text: "꼭 다 가져가야만 속이 후련했냐!", actor: "김래원", character: "오태식", movie: "해바라기", audio: "se_009.m4a" },
